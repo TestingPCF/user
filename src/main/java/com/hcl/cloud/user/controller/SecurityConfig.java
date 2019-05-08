5bdb6ca6514c35e23a74d351080e1a378c33ef2a
Dinesh Kumar Sharma <dineshkumar.sharma@HCL.COM>
2019-05-08 14:24:06 +0530

    config server change

--- src/main/java/com/hcl/cloud/user/controller/SecurityConfig.java
+++ src/main/java/com/hcl/cloud/user/controller/SecurityConfig.java
@@ -6,7 +6,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
+//change
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
