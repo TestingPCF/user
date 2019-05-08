4908aa41d4cae17c7bab2f63eff7974b66b33e98
Dinesh Kumar Sharma <dineshkumar.sharma@HCL.COM>
2019-05-08 14:33:02 +0530

    config server change

--- src/main/java/com/hcl/cloud/user/controller/SecurityConfig.java
+++ src/main/java/com/hcl/cloud/user/controller/SecurityConfig.java
@@ -1,6 +1,6 @@
 package com.hcl.cloud.user.controller;
 
-import org.springframework.context.annotation.Configuration;
+/*import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -12,4 +12,4 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.csrf().disable();
         http.authorizeRequests().anyRequest().permitAll();
     }
-}
+}*/
